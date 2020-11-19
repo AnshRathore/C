576c02b46094e5c9fd7d570e3453431bee0aa816
AnshRathore <74612896+AnshRathore@users.noreply.github.com>
2020-11-19 13:22:13 +0530

    Initial commit

---
+++ hello2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main ()
+{
+
+    printf( "hello love!\n" );
+
+    printf( "who this\n" );
+
+    printf( "ouch has it been that long\n");
+
+    printf( "%f",3.14 );
+
+    printf( "\n %s","07");
+
+}
