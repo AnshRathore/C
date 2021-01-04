80d6419592a17086d9fb1c85b1522fc75e74d974
AnshRathore <74612896+AnshRathore@users.noreply.github.com>
2021-01-04 12:23:50 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = -1;
+
+    unsigned int j = 100;
+
+    if(j>i){
+
+    printf("100 is greater\n");
+    
+    }
+
+    else{
+
+        printf("-1 is greater than 100");
+    }
+}
+
